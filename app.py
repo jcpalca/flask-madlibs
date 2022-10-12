17f6ed420798a11a46799f90e3c14f4eef4bf8c2
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-12 16:33:14 -0700

    Full madlibs functionality

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story as story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/questions")
+def questions():
+    word_types = story.prompts
+    return render_template("questions.html", word_types = word_types)
+
+@app.get("/results")
+def results():
+    full_story = story.generate(request.args)
+    return render_template("results.html", full_story = full_story)
+
